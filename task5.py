245559e85ab969840f86ff1d27527022973bf345
Mr Emperor <mremperor003@gmail.com>
2025-07-24 16:19:31 +0000

    update

--- task5.py
+++ task5.py
@@ -10,3 +10,34 @@
     6. Find out what position "Paul" is in your final friends list (remember: position counting starts from 1 for humans!).
     7. arrange your contacts in Descending Alphabetical Order using.
 """
+# task 5	
+# Initial friends list
+friends = ["Aisha", "Daniel", "Esther", "John", "Mary", "Paul", "Ruth"]
+
+# 1. Add "Kemi" between "John" and "Mary"
+
+friends.insert(3, "Kemi")
+print(friends)
+# 2. Remove "Daniel"
+friends.remove(friends[1])
+print(friends)
+# 3. Update "Aisha" to "Aisha_M"
+
+friend[0]=("Aisha_M")
+print(friends)
+
+# 4. Add "Zainab" at the end
+friends.append("Zainab")
+print(friends)
+# 5. New list with only the first 3 friends
+close_three = friends[:3]
+print("First 3 close friends:", close_three)
+
+# 6. Find position of "Paul" (starting from 1)
+paul_position = friends.index("Paul") + 1
+print("Paul's position in the list:", paul_position)
+
+# 7. Arrange (sort) the final friends list alphabetically
+friends.sort()
+print("Alphabetically sorted friends list:", friends)
+
